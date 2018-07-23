a8f9186757ae809dde3eb997455adac8c24857e2
Silvana\Silvana <silvana.malan@epn.edu.ec>
2018-07-22 22:47:57 -0500

    bd

--- project/src/app.module.ts
+++ project/src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import {PacienteController} from "./paciente/paciente.controller";
-import {MedicamentosController} from "./medicamentos/medicamentos.controller";
-import {PacienteService} from "./paciente/paciente.service";
-import {MedicamentoService} from "./medicamentos/medicamento.service";
-import {AutorizacionController} from "./autorizacion.controller";
+import {PacienteController} from './paciente/paciente.controller';
+import {MedicamentosController} from './medicamentos/medicamentos.controller';
+import {PacienteService} from './paciente/paciente.service';
+import {MedicamentoService} from './medicamentos/medicamento.service';
+import {AutorizacionController} from './autorizacion.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PacienteEntity } from './paciente/paciente.entity';
 import { MedicamentoEntity } from './medicamentos/medicamento.entity';
@@ -29,7 +29,7 @@ import { PeticionesService } from './Peticiones/peticiones.service';
       synchronize: true,
       ssl: true,
     }),
-    TypeOrmModule.forFeature([PacienteEntity,MedicamentoEntity, UsuarioEntity, PeticionesEntity])
+    TypeOrmModule.forFeature([PacienteEntity, MedicamentoEntity, UsuarioEntity, PeticionesEntity]),
   ],
   controllers: [AppController, PacienteController, MedicamentosController, AutorizacionController, UsuarioController, PeticionesController ],
   providers: [AppService, PacienteService, MedicamentoService, UsuarioService, PeticionesService],
